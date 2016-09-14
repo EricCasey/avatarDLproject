f7fa82d11e60b47f290b710af7646704e3587dd2
EricCasey <ericcasey509@gmail.com>
2016-09-14 12:56:17 -0400

    initial repo creation & commit

---
+++ download_avatar.js
@@ -0,0 +1,78 @@
+//TESTED USING http://api.github.com/repos/chrislgarry/Apollo-11/contributors
+
+// run --> $ node download_avatar.js Apollo-11 chrislgarry
+
+// .env    token format --> BEARER_TOKEN=...
+
+require('dotenv').config() //check .env file
+var request = require('request'), //  request library
+    fs = require('fs'), //  file system library
+    chalk = require('chalk'), // for coloring results
+    clear = require('clear'), // this clears the screen
+    newModule = require('./my-module'); // access with newModule.key
+
+
+
+
+
+function getDLRepoContribz(repoOwner, repoName, callback) {
+    clear(); // get ready for a show
+
+    var target = `https://api.github.com/repos/${repoName}/${repoOwner}/contributors`;
+    var requestData = {
+        url: target,
+        auth: {
+            bearer: process.env.BEARER_TOKEN, // token saved in newModule (my-module.js)
+        },
+        headers: {
+            'User-Agent': 'TRON',
+        }
+    };
+
+    var fileCount = 0;
+
+    console.log(chalk.yellow(`1 CRAFTING REQUEST!\n*`));
+
+    request.get(requestData, (err, response, body) => {
+
+        var data = JSON.parse(body); // parses JSON data for formatting / readability
+
+        if (response.statusCode === 401) {
+            throw Error(`ACCESS DENIED!\n`);
+        } else if (!fs.existsSync(newModule.env)) {
+            throw Error(`YOU DONT HAVE A .env FILE!`);
+        };
+
+        if (process.argv.length < 2 || process.argv[3] === undefined) {
+            throw Error(chalk.red(`TOO FEW ARGUMENTS \:\(\nTry putting in more arguments\n$ node download_avatar.js <REPO> <USERNAME>`));
+        } else if (process.argv[4] !== undefined) {
+            throw Error(chalk.red(`TOO MANY ARGUMENTS \:\(\nTry putting in fewer arguments\n$ node download_avatar.js <REPO> <USERNAME>`));
+        } else if (data.message == 'Not Found') {
+            throw Error(chalk.red(`Repo or Owner Does Not Exist \:\(\n$ node download_avatar.js <REPO> <USERNAME>`));
+        } else {
+            for (var i = 0; i <= data.length - 1; i++) {
+                if (i == data.length - 1) {
+                    callback(chalk.yellow(`2 PROFILE IMAGE URLs ACQUIRED!\n**`)); // yay!
+                }
+                var url = data[i].avatar_url; // url of image is saves as url
+                var filename = `${data[i].login}.png` // name saved to filename
+                var username = data[i].login // saves username to username
+                newModule.download(url, filename, function() {
+                        fileCount++;
+                        console.log(`4.${fileCount} -- ${Math.round((fileCount / data.length) * 100)}%`);
+                    }) // calls the download function from the module
+            }; // end of for loop url bulder loop
+        }; // end of else
+        console.log(chalk.yellow(`3 DOWNLOADING ${data.length} IMAGES!\n***\n ${chalk.red('#')} --- ${chalk.red('%')}`))
+    }); // end of request.get
+}; // end of getDLRepoContribz
+
+getDLRepoContribz(process.argv[2], process.argv[3], console.log);
+
+
+// NEXT STEPS <--
+//> Add Mocha & Chai tests
+//> Align %downloaded values
+//> Add something that counts the file size
+//> Add regex input-checker
+//>
---
+++ my-module.js
@@ -0,0 +1,25 @@
+var fs = require('fs'),
+    request = require('request'),
+    chalk = require('chalk'); //for coloring results
+
+module.exports = {
+    env : '.env',
+    directory: 'avatars',
+    download: function(url, filename, callback2) { // it won't let me declare this function with => for some reason.
+
+        if (!fs.existsSync(this.directory)) {
+            fs.mkdirSync(this.directory);
+            console.log(chalk.yellow(`1.1 AVATARS DIRECTORY CREATED!\n*`));
+         }
+
+         request.head(url, function(err, res, body) {
+             request(url).pipe(fs.createWriteStream(`avatars/${filename}`)).on('close', callback2);
+
+         });
+    }
+}
+
+
+// NEXT STEPS <--
+//> Add file system i.e. avatars/username/repo/file.png
+//>
